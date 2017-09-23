6fc468d6db36b29f97368360df9552e4996d1a23
Kevin Mårtensson <kevinmartensson@gmail.com>
2017-09-23 09:54:25 +0200

    Add `https` option (#8)

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 import test from 'ava';
-import m from './';
+import m from '.';
 
-test(t => {
+test('prepend http', t => {
 	t.is(m('todomvc.com'), 'http://todomvc.com');
 	t.is(m('http://todomvc.com'), 'http://todomvc.com');
 	t.is(m('https://todomvc.com'), 'https://todomvc.com');
@@ -14,4 +14,10 @@ test(t => {
 	t.is(m('/relative'), '/relative');
 	t.is(m('mailto:info@site.com'), 'mailto:info@site.com');
 	t.is(m('tel:1234567890'), 'tel:1234567890');
+});
+
+test('https option', t => {
+	t.is(m('todomvc.com', {https: true}), 'https://todomvc.com');
+	t.is(m('//todomvc.com', {https: true}), '//todomvc.com');
+	t.is(m('localhost:8000', {https: true}), 'https://localhost:8000');
 });
